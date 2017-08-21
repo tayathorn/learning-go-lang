456b61e3b31e22ae0648918fc67249ae7bfefd7c
tayathorn <tayathorn@thinknet.co.th>
2017-08-21 18:35:07 +0700

    Commenting in array

--- basic/try.go
+++ basic/try.go
@@ -5,21 +5,33 @@ import "fmt"
 func main() {
 	/*================== Array ==================*/
 
-	var datas [10]string // ตัวแปร data , length = 10 , type string
-	datas[0] = "Zero"
-	datas[1] = "One"
-	datas[2] = "Two"
-	datas[3] = "Three"
-	datas[4] = "Four"
-	datas[5] = "Five"
-	datas[6] = "Six"
+	// var datas [10]string // ตัวแปร data , length = 10 , type string
+	// datas[0] = "Zero"
+	// datas[1] = "One"
+	// datas[2] = "Two"
+	// datas[3] = "Three"
+	// datas[4] = "Four"
+	// datas[5] = "Five"
+	// datas[6] = "Six"
 
-	for _, v := range datas {
-		fmt.Printf("%v\n", v)
+	// for _, v := range datas {
+	// 	fmt.Printf("%v\n", v)
+	// }
+
+	// fmt.Println(datas[1])
+	// fmt.Println(datas[:])
+	// fmt.Println(datas[2:5]) // print range of array
+
+	//--------- Commenting in array ----------
+	x := [4]int{
+		1,
+		2,
+		3,
+		// 4,
+		5,
 	}
 
-	fmt.Println(datas[1])
-	fmt.Println(datas[:])
-	fmt.Println(datas[2:5]) // print range of array
+	fmt.Println(len(x))
+	fmt.Println(x[:])
 	/*============================================*/
 }

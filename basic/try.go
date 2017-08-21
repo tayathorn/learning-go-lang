d36e871ef8a8cdce8a080722a14b13d6b7c1d1c7
tayathorn <tayathorn@thinknet.co.th>
2017-08-21 18:29:57 +0700

    group variable

--- basic/try.go
+++ basic/try.go
@@ -6,15 +6,24 @@ const name = "Hello"
 
 func main() {
 	// variable type
-	i := 1
-	f := 1.0
-	s := "Hello"
-	b := true
+	/*============================================*/
+	// i := 1
+	// f := 1.0
+	// s := "Hello"
+	// b := true
 
-	fmt.Printf("Type %T has value %d\n", i, i) //integer
-	fmt.Printf("Type %T has value %f\n", f, f) //float
-	fmt.Printf("Type %T has value %s\n", s, s) //string
-	fmt.Printf("Type %T has value %t\n", b, b) //boolean
+	// fmt.Printf("Type %T has value %d\n", i, i)
+	// fmt.Printf("Type %T has value %f\n", f, f)
+	// fmt.Printf("Type %T has value %s\n", s, s)
+	// fmt.Printf("Type %T has value %t\n", b, b)
+	/*============================================*/
 
-	fmt.Printf("%T => %v\n", name, name)
+	// fmt.Printf("%T => %v\n", name, name)
+
+	/*============== Group Variable ==============*/
+	i, j := 1, 2
+
+	fmt.Printf("%T => %v\n", i, i)
+	fmt.Printf("%T => %v\n", j, j)
+	/*============================================*/
 }

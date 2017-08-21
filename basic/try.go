31185e845a095fd2cd779093c38041dbb425f220
tayathorn <tayathorn@thinknet.co.th>
2017-08-21 18:39:44 +0700

    Check data in map

--- basic/try.go
+++ basic/try.go
@@ -9,8 +9,19 @@ func main() {
 	datas["lastname"] = "Pan"
 	datas["gender"] = "Female"
 
-	for k, value := range datas {
-		fmt.Printf("%v => %v\n", k, value)
+	// for k, value := range datas {
+	// 	fmt.Printf("%v => %v\n", k, value)
+	// }
+
+	// //--------- Check data in map ----------
+	fmt.Println("---------------")
+	if datas["firstname"] == "" {
+		fmt.Println(datas["firstname"])
 	}
+
+	if data, ok := datas["firstname"]; ok { // ok ข้างหลังคือ condition แบ่งโดย ; ข้างหน้าคือ statement
+		fmt.Printf("\n%s : %t\n", data, ok)
+	}
+
 	/*============================================*/
 }

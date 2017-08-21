c28326f623c1238ab9e1115456abda342693a576
tayathorn <tayathorn@thinknet.co.th>
2017-08-21 18:39:11 +0700

    basic map

--- basic/try.go
+++ basic/try.go
@@ -3,25 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	/*============== Loop ==============*/
-	// for i := 1; i < 10; i++ {
-	// 	fmt.Println(i)
-	// }
+	/*=================== Map ======================*/
+	datas := make(map[string]string) //map[key:string]value:string
+	datas["firstname"] = "Tayathorn"
+	datas["lastname"] = "Pan"
+	datas["gender"] = "Female"
 
-	//--------- or ----------
-	// i := 1
-	// for i < 10 {
-	// 	fmt.Println(i)
-	// 	i++
-	// }
-
-	//--------- or ----------
-	i := 0
-	for {
-		fmt.Println("GGGG")
-		if i++; i > 10 {
-			break
-		}
+	for k, value := range datas {
+		fmt.Printf("%v => %v\n", k, value)
 	}
 	/*============================================*/
 }

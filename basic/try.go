fa977c1d8b84f1a7971064be0d70f83ac3d5001c
tayathorn <tayathorn@thinknet.co.th>
2017-08-21 18:37:56 +0700

    Slice : Append, Copy

--- basic/try.go
+++ basic/try.go
@@ -5,11 +5,23 @@ import "fmt"
 func main() {
 	/*============== Slice =======================*/
 
-	var datas = make([]string, 3)
-	datas[0] = "Zero"
-	datas[1] = "One"
-	datas[2] = "Two"
+	// var datas = make([]string, 3)
+	// datas[0] = "Zero"
+	// datas[1] = "One"
+	// datas[2] = "Two"
 
-	fmt.Println(datas[:])
+	// fmt.Println(datas[:])
+
+	//--------- Slice : Append, Copy ----------
+
+	slice1 := []int{1, 2, 3}
+	slice2 := append(slice1, 4, 5)
+	fmt.Printf("%v\n", slice1)
+	fmt.Printf("%v\n", slice2)
+
+	slice3 := []int{1, 2, 3}
+	slice4 := make([]int, 2)
+	copy(slice4, slice3)
+	fmt.Println(slice3, slice4)
 	/*============================================*/
 }
